41d72106e42bcce48a513223d2d22099f8c9a2e0
Milo Roche-Vandenbroucque <milo.roche@outlook.com>
2023-10-05 14:00:32 +0200

    Added quizz et json-server

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,31 @@
 import Counter from "../Counter/Counter";
+import data from "../../db.json";
 
 function Main() {
-    return (
-        <section className="main-container">
-            <Counter />
-        </section>
-    )
+  console.log(data.quiz);
+
+  return (
+    <section className="main-container">
+      <Counter />
+      {data.quiz.map((quiz) => {
+        return (
+          <div>
+            <h1>{quiz.question}</h1>
+            {
+              quiz.options.map((answer) => {
+                return (
+                  <div>
+                    <p>{answer}</p>
+                  </div>
+                );
+              }
+              )
+            }
+          </div>
+        );
+      })}
+    </section>
+  );
 }
 
 export default Main;
